a5dcf5146a98add3c0de9da4b93e8aa4892f6aee
sc0ttms <sc0ttms@163.com>
2022-01-13 13:47:29 +0800

    feat: add offline_laplace_norm and cumulative_laplace_norm on feature.py

--- audio/feature.py
+++ audio/feature.py
@@ -37,3 +37,42 @@ def sub_sample(noisy, clean, samples):
     assert len(noisy) == len(clean) == samples
 
     return noisy, clean
+
+
+def offline_laplace_norm(input):
+    """
+    Args:
+        input: [B, C, F, T]
+    Returns:
+        [B, C, F, T]
+    """
+    # utterance-level mu
+    mu = torch.mean(input, dim=list(range(1, input.dim())), keepdim=True)
+
+    normed = input / (mu + 1e-5)
+
+    return normed
+
+
+def cumulative_laplace_norm(input):
+    """
+    Args:
+        input: [B, C, F, T]
+    Returns:
+    """
+    batch_size, num_channels, num_freqs, num_frames = input.size()
+    input = input.reshape(batch_size * num_channels, num_freqs, num_frames)
+
+    step_sum = torch.sum(input, dim=1)  # [B * C, F, T] => [B, T]
+    cumulative_sum = torch.cumsum(step_sum, dim=-1)  # [B, T]
+
+    entry_count = torch.arange(num_freqs, num_freqs * num_frames + 1, num_freqs, dtype=input.dtype, device=input.device)
+    entry_count = entry_count.reshape(1, num_frames)  # [1, T]
+    entry_count = entry_count.expand_as(cumulative_sum)  # [1, T] => [B, T]
+
+    cumulative_mean = cumulative_sum / entry_count  # B, T
+    cumulative_mean = cumulative_mean.reshape(batch_size * num_channels, 1, num_frames)
+
+    normed = input / (cumulative_mean + 1e-7)
+
+    return normed.reshape(batch_size, num_channels, num_freqs, num_frames)
